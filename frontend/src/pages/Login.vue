94547e459eafc7da3a252016b242479bd36bc2e1
Jan-Hendrik du Plessis <70935651+Slyfox688@users.noreply.github.com>
2022-08-25 20:29:35 +0200

    added login

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,3 +1,41 @@
-<template>
-<div>login page</div>
+<template class="background">
+  <div class="LG-body">
+    <h1>Login</h1>
+    <FormKit type="form" submit-label="Login" @submit="login">
+      <FormKitSchema :schema="Login_data" />
+      
+    </FormKit>
+  </div>
+
 </template>
+
+<script>
+//import { FormKit } from '@formkit/vue'
+import { FormKitSchema } from '@formkit/vue'
+import Login_data from '../config/Login.json'
+
+export default {
+  name: 'Login',
+  components: {
+    FormKitSchema
+  },
+  data() {
+    return {
+      Login_data
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style>
+.LG-body {
+  position: absolute;
+  top: 30%;
+  left: 38%;
+  width: 20%;
+  height: 20%;
+}
+</style>

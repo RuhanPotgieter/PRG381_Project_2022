7084c8ca32b813752d87c76b1f8f1adbe0c4fddb
Juan-Roux <576984@student.belgiumcampus.ac.za>
2022-08-28 20:41:01 +0200

    Added @RequestMapping

--- DataAccessLayer/spring_boot/src/main/java/com/register/spring_boot/RegisterController.java
+++ DataAccessLayer/spring_boot/src/main/java/com/register/spring_boot/RegisterController.java
@@ -4,24 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegisterController {
     @Autowired
     private RegisterService service;
      
+    @RequestMapping("/api/getAllRegister")
     public List<Register> listAll() {
         return service.listAll();
     }
      
+    @RequestMapping("/api/saveRegister")
     public void save(Register admin) {
         service.save(admin);
     }
      
+    @RequestMapping("/api/getRegister")
     public Register get(long id) {
         return service.get(id);
     }
      
+    @RequestMapping("/api/deleteRegister")
     public void delete(long id) {
         service.delete(id);
     }
